35bc8d4c5f70d8dc6e9911d646adab26093c3b2b
SimonRJenkins <105888107+SimonRJenkins@users.noreply.github.com>
2023-08-20 19:10:10 +0100

    Improvements
    Changed margin so that the body is in the center and there is space at the top of the page
    Grouped selectors
    Removed test function
    Made characters array empty as it was full of individual characters, and added a loop that loops through ASCII character codes to give the characters instead
    Tidied up some layout/formatting
    Adjusted line height

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
-const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
-    "/"];
+const characters = [];
 
-let box1 = document.getElementById("pw-box1-text")
-let box2 = document.getElementById("pw-box2-text")
-
-function test() {
-    console.log("test")
-    box1.textContent = "box1"
-    box2.textContent = "box2"
+for (let i = 33; i <= 126; i++) {
+  characters.push(String.fromCharCode(i));
 }
+
+let box1 = document.getElementById("pw-box1-text");
+let box2 = document.getElementById("pw-box2-text");
 
 function generatePasswords() {
-    let firstPassword = "";
+  let firstPassword = "";
+  let secondPassword = "";
 
-    for (let i = 0; i < 15; i++) {
-        firstPassword += characters[Math.floor(Math.random() * characters.length)]
-    }
-    box1.textContent = firstPassword;
+  for (let i = 0; i < 15; i++) {
+    firstPassword += characters[Math.floor(Math.random() * characters.length)];
+    secondPassword += characters[Math.floor(Math.random() * characters.length)];
+  }
 
-    let secondPassword = "";
-    for (let i = 0; i < 15; i++) {
-        secondPassword += characters[Math.floor(Math.random() * characters.length)]
-    }
-    box2.textContent = secondPassword;
+  box1.textContent = firstPassword;
+  box2.textContent = secondPassword;
 }
-
