cce10f9e122873fa9973214789ec36677729400a
SimonRJenkins <105888107+SimonRJenkins@users.noreply.github.com>
2023-10-02 14:44:59 +0100

    Copy function
    By adding navigator.clipboard.writeText , the user can click the copy icon under the corresponding password box to have the password copied to their clipboard

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const characters = [];
 
+//33-47 symbols
+//48-57 numbers
+//58-64 symbols
+//65-90 letters
+//91-96 symbols
+//97-122 letters
+//123-126 symbols
+
 for (let i = 33; i <= 126; i++) {
   characters.push(String.fromCharCode(i));
 }
 
 let box1 = document.getElementById("pw-box1-text");
 let box2 = document.getElementById("pw-box2-text");
+
+let copyBox1 = document.getElementById("copy1")
+let copyBox2 = document.getElementById("copy2")
 
 function generatePasswords() {
   let firstPassword = "";
@@ -18,4 +29,16 @@ function generatePasswords() {
 
   box1.textContent = firstPassword;
   box2.textContent = secondPassword;
+
+  copyBox1.style.display = "flex"
+  copyBox2.style.display = "flex"
 }
+
+
+copyBox1.addEventListener('click', function() {
+  navigator.clipboard.writeText(box1.textContent)
+})
+
+copyBox2.addEventListener('click', function() {
+  navigator.clipboard.writeText(box2.textContent)
+})
