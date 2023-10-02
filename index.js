aa980198333c3e4b10fea5020ac40bbcec960bf0
SimonRJenkins <105888107+SimonRJenkins@users.noreply.github.com>
2023-10-02 16:55:25 +0100

    Password length
    Added number input for password length that is between 8 and 15 characters
    The value is checked and then the passwords are generated with the selected length
    Fixed styling and combined selectors

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ let box2 = document.getElementById("pw-box2-text");
 let copyBox1 = document.getElementById("copy1")
 let copyBox2 = document.getElementById("copy2")
 
-const numbers = document.getElementById("numbers")
-const symbols = document.getElementById("symbols")
+let numbers = document.getElementById("numbers")
+let symbols = document.getElementById("symbols")
+
+let passwordLength = document.getElementById("length")
 
 function generatePasswords() {
 
@@ -43,20 +45,16 @@ function generatePasswords() {
   let firstPassword = "";
   let secondPassword = "";
 
-  for (let i = 0; i < 15; i++) {
+  for (let i = 0; i < passwordLength.value; i++) {
     firstPassword += characters[Math.floor(Math.random() * characters.length)];
     secondPassword += characters[Math.floor(Math.random() * characters.length)];
   }
-
-  //set 8-15 length
 
   box1.textContent = firstPassword;
   box2.textContent = secondPassword;
 
   copyBox1.style.display = "flex"
   copyBox2.style.display = "flex"
-
-
 
   copyBox1.addEventListener('click', function () {
     navigator.clipboard.writeText(box1.textContent)
