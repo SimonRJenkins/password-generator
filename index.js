9f89f086b10ff1a33fb44114c02d45ace2ec26a3
SimonRJenkins <105888107+SimonRJenkins@users.noreply.github.com>
2023-10-03 19:58:50 +0100

    Code review improvements
    Removed CSS that wasn't needed after looking at my code review
    Removed the second of the letter loops and included .toLowerCase to the first loop instead
    Removed the symbols.checked for loops and replaced with an array of symbols, as there is not enough symbols to need 4 separate for loops
    changed box and copybox to consts as they are not reassigned

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-let box1 = document.getElementById("pw-box1-text");
-let box2 = document.getElementById("pw-box2-text");
+const box1 = document.getElementById("pw-box1-text");
+const box2 = document.getElementById("pw-box2-text");
 
-let copyBox1 = document.getElementById("copy1")
-let copyBox2 = document.getElementById("copy2")
+const copyBox1 = document.getElementById("copy1")
+const copyBox2 = document.getElementById("copy2")
 
 let numbers = document.getElementById("numbers")
 let symbols = document.getElementById("symbols")
@@ -11,14 +11,10 @@ let passwordLength = document.getElementById("length")
 
 function generatePasswords() {
 
-  const characters = [];
+  let characters = [];
 
   for (let i = 65; i <= 90; i++) {
-    characters.push(String.fromCharCode(i))
-  }
-
-  for (let i = 97; i <= 122; i++) {
-    characters.push(String.fromCharCode(i))
+    characters.push(String.fromCharCode(i), String.fromCharCode(i).toLowerCase())
   }
 
   if (numbers.checked) {
@@ -28,18 +24,7 @@ function generatePasswords() {
   }
 
   if (symbols.checked) {
-    for (let i = 33; i <= 47; i++) {
-      characters.push(String.fromCharCode(i))
-    }
-    for (let i = 58; i <= 64; i++) {
-      characters.push(String.fromCharCode(i))
-    }
-    for (let i = 91; i <= 96; i++) {
-      characters.push(String.fromCharCode(i))
-    }
-    for (let i = 123; i <= 126; i++) {
-      characters.push(String.fromCharCode(i))
-    }
+    characters = [...characters, ...'!#$%&()*+,-./:;<=>?@[\\\'\\"]^_`']
   }
 
   let firstPassword = "";
